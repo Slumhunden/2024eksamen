213dfb0fd8a0078d89b1e19a55748fa69e5f0eee
Slumhunden <kroghbastin@gmail.com>
2024-06-20 21:51:40 +0200

    compiles
    ifølge alt jeg kan finde er koden korrekt, den vil dog stadig ikke poste/put på deltager

--- src/main/java/eksamen/atletik/config/WebConfig.java
+++ src/main/java/eksamen/atletik/config/WebConfig.java
@@ -1,8 +1,13 @@
 package eksamen.atletik.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 // Enables CORS (Cross-Origin Resource Sharing) for the entire application
 @Configuration
@@ -14,5 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
+    }
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        return new MappingJackson2HttpMessageConverter();
+    }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
     }
 }
